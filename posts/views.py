dc9489fe21f86c1b029ca50eb35795a181396dbd
Peer Naupold <greenion@gmx.de>
2020-05-04 01:19:57 +0200

    formatting corrected in views.py

--- posts/views.py
+++ posts/views.py
@@ -3,16 +3,17 @@ from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
 
+
 def get_posts(request):
     """
     Create a view that will return a list
     of Posts that were published prior to 'now'
     and render them to the 'blogposts.html' template
     """
-
-    posts = Post.objects.filter(published_date__lte=timezone.now
-        ()).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+        ).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
+
 
 def post_detail(request, pk):
     """
@@ -20,12 +21,13 @@ def post_detail(request, pk):
     Post object based on the post ID (pk) and
     render it to the 'postdetail.html' template.
     Or return a 404 error if the post is
-    not found.
+    not found
     """
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
     post.save()
     return render(request, "postdetail.html", {'post': post})
+
 
 def create_or_edit_post(request, pk=None):
     """
